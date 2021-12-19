2d9014ccc6048f85d5cec139d5e46510bc4c0100
Bunrealbo <vuthithuydung_t64@hus.edu.vn>
2021-12-20 04:44:43 +0700

    base ui register activity

--- app/src/main/java/com/longng/blog4pp/RegisterActivity.java
+++ app/src/main/java/com/longng/blog4pp/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.longng.blog4pp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(
+            R.id.toolbar
+    )
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        toolbar.setNavigationOnClickListener((view) -> finish());
     }
 }
