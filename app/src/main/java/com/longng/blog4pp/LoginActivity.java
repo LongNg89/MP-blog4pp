c2d1f8024a957dfb20f590e71423f334e5139b0d
Bunrealbo <vuthithuydung_t64@hus.edu.vn>
2021-12-20 05:11:40 +0700

    hander base in login activity

--- app/src/main/java/com/longng/blog4pp/LoginActivity.java
+++ app/src/main/java/com/longng/blog4pp/LoginActivity.java
@@ -2,13 +2,51 @@ package com.longng.blog4pp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(
+            R.id.et_email
+    )
+    EditText etEmail;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(
+            R.id.et_password
+    )
+    EditText etPassword;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(
+            R.id.bt_login
+    )
+    Button btLogin;
+
+    @SuppressLint("NonConstantResourceId")
+    @BindView(
+            R.id.bt_create_new_account
+    )
+    Button btCreateNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+
+        btCreateNewAccount.setOnClickListener((view) -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
     }
 }
