b5bb97ed0c5f0a561c812cfe388c1991d2e75114
Bunrealbo <vuthithuydung_t64@hus.edu.vn>
2021-12-20 20:32:39 +0700

    handler base login

--- app/src/main/java/com/longng/blog4pp/LoginActivity.java
+++ app/src/main/java/com/longng/blog4pp/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.longng.blog4pp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -44,8 +46,41 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        btLogin.setOnClickListener((view) -> {
+            final String email = etEmail.getText().toString();
+            final String password = etPassword.getText().toString();
+            try {
+                FirebaseAuth
+                        .getInstance()
+                        .signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener((authResult) -> {
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        })
+                        .addOnFailureListener(command -> {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                            builder.setMessage(command.getMessage());
+                            builder.setTitle(R.string.error);
+                            builder.setPositiveButton(R.string.ok, (dialog, id) -> dialog.cancel());
+                            builder.setNegativeButton(R.string.cancel, (dialog, id) -> dialog.cancel());
+                            AlertDialog dialog = builder.create();
+                            dialog.show();
+                        });
+            } catch (Exception e) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setMessage(e.getMessage());
+                builder.setTitle(R.string.error);
+                builder.setPositiveButton(R.string.ok, (dialog, id) -> dialog.cancel());
+                builder.setNegativeButton(R.string.cancel, (dialog, id) -> dialog.cancel());
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
-        btCreateNewAccount.setOnClickListener((view) -> {
+        btCreateNewAccount.setOnClickListener((view) ->
+
+        {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
